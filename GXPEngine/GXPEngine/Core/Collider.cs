816e561026e33d051579f9531ae2ddfff4548226
caspar LOL <547366@student.saxion.nl>
2024-03-11 14:20:31 +0100

    Add raycast to Collider

--- GXPEngine/GXPEngine/Core/Collider.cs
+++ GXPEngine/GXPEngine/Core/Collider.cs
@@ -27,30 +27,42 @@ namespace GXPEngine.Core
 		public virtual bool HitTest (Collider other) {
 			return false;
 		}
-		
-		//------------------------------------------------------------------------------------------------------------------------
-		//														HitTest()
-		//------------------------------------------------------------------------------------------------------------------------		
-		/// <summary>
-		/// Returns <c>true</c> if this collider is currently overlapping with the point x,y.
-		/// </summary>
-		public virtual bool HitTestPoint (float x, float y, float z) {
-			return false;
-		}
 
-		/// <summary>
-		/// If this collider would collide with collider other after moving by vx,vy,
-		/// then this method returns the time of impact of the collision, which is a number between 
-		/// 0 (=immediate collision, or already overlapping) and 1 (=collision after moving exactly by vx,vy).
-		/// Otherwise, a number larger than 1 (e.g. float.MaxValue) is returned.
-		/// In addition, the collision normal is returned, in case of a collision.
-		/// </summary>
-		/// <returns>The time of impact.</returns>
-		/// <param name="other">Another collider.</param>
-		/// <param name="vx">x velocity or translation amount.</param>
-		/// <param name="vy">y velocity or translation amount.</param>
-		/// <param name="normal">The collision normal.</param>
-		public virtual float TimeOfImpact (Collider other, float vx, float vy, float vz, out Vector3 normal) {
+        //------------------------------------------------------------------------------------------------------------------------
+        //														HitTest()
+        //------------------------------------------------------------------------------------------------------------------------		
+        /// <summary>
+        /// Returns <c>true</c> if this collider is currently overlapping with the point x,y.
+        /// </summary>
+        public virtual bool HitTestPoint(float x, float y, float z)
+        {
+            return false;
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------
+        //														RayCastTest()
+        //------------------------------------------------------------------------------------------------------------------------		
+        /// <summary>
+        /// Returns <c>true</c> if this collider is currently overlapping with the ray from point start to end.
+        /// </summary>
+        public virtual bool RayCastTest(Vector3 start, Vector3 end)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// If this collider would collide with collider other after moving by vx,vy,
+        /// then this method returns the time of impact of the collision, which is a number between 
+        /// 0 (=immediate collision, or already overlapping) and 1 (=collision after moving exactly by vx,vy).
+        /// Otherwise, a number larger than 1 (e.g. float.MaxValue) is returned.
+        /// In addition, the collision normal is returned, in case of a collision.
+        /// </summary>
+        /// <returns>The time of impact.</returns>
+        /// <param name="other">Another collider.</param>
+        /// <param name="vx">x velocity or translation amount.</param>
+        /// <param name="vy">y velocity or translation amount.</param>
+        /// <param name="normal">The collision normal.</param>
+        public virtual float TimeOfImpact (Collider other, float vx, float vy, float vz, out Vector3 normal) {
 			normal = new Vector3 ();
 			return float.MaxValue;
 		}
