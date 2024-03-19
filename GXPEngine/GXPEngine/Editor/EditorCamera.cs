defddd8fb170002a3996d38a20472e196a60d876
caspar LOL <547366@student.saxion.nl>
2024-03-19 20:12:02 +0100

    Finalize editor camera

--- GXPEngine/GXPEngine/Editor/EditorCamera.cs
+++ GXPEngine/GXPEngine/Editor/EditorCamera.cs
@@ -22,26 +22,57 @@ namespace GXPEngine.Editor
 
         void Update()
         {
-            SetRotation();
+            UpdateRotation();
+            UpdatePosition();
         }
 
-        void SetRotation()
+        public Vector3 ScreenPointToGlobal(int screenX, int screenY, float depth)
+        {
+            return actualCam.ScreenPointToGlobal(screenX, screenY, depth);
+        }
+
+        void UpdatePosition()
+        {
+            //this breaks if EditorCamera is a child of anything (rotated or scaled). Good thing that its only allowed in the editor
+            float vel = Time.deltaTimeS;
+            if (Input.GetKey(Key.LEFT_SHIFT)) vel *= 4;
+            if (Input.GetKey(Key.W) || Input.GetKey(Key.S))
+            {
+                float vec = Input.GetKey(Key.S) ? vel : 0;
+                vec -= Input.GetKey(Key.W) ? vel : 0;
+                position += actualCam.TransformDirection(0, 0, vec);
+            }
+            if (Input.GetKey(Key.A) || Input.GetKey(Key.D))
+            {
+                float vec = Input.GetKey(Key.D) ? vel : 0;
+                vec -= Input.GetKey(Key.A) ? vel : 0;
+                position += TransformDirection(vec, 0, 0);
+            }
+            if (Input.GetKey(Key.SPACE) || Input.GetKey(Key.LEFT_CTRL))
+            {
+                float vec = Input.GetKey(Key.SPACE) ? vel : 0;
+                vec -= Input.GetKey(Key.LEFT_CTRL) ? vel : 0;
+                y += vec;
+            }
+        }
+
+        void UpdateRotation()
         {
             if (Input.GetMouseButton(1))
             {
                 mouseVelocity = new Vector2(Input.mouseX, Input.mouseY) - mousePosition;
 
                 screenRotation += mouseVelocity * Time.deltaTimeS;
-                if (screenRotation.y > Mathf.PI) screenRotation.y = Mathf.PI * .999f;
-                if (screenRotation.y < -Mathf.PI) screenRotation.y = Mathf.PI * -.999f;
+                if (screenRotation.y > .5f*Mathf.PI) screenRotation.y = Mathf.PI * .499f;
+                if (screenRotation.y < -.5f * Mathf.PI) screenRotation.y = Mathf.PI * -.499f;
 
                 screenRotation.x %= 2 * Mathf.PI;
+                rotation = Quaternion.FromRotationAroundAxis(Vector3.up, screenRotation.x);
+
+                actualCam.rotation = Quaternion.FromRotationAroundAxis(Vector3.left, screenRotation.y);
             }
 
             mousePosition = new Vector2(Input.mouseX, Input.mouseY);
-
-            rotation = Quaternion.FromRotationAroundAxis(Vector3.up, screenRotation.x);
-            actualCam.rotation = Quaternion.FromRotationAroundAxis(Vector3.left, screenRotation.y);
         }
     }
 }
