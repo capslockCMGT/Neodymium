177e88ee4d87cae82a999b527b98cf4fc5b75f16
Tetroit <shichinov.ivan@gmail.com>
2024-04-02 19:01:50 +0200

    code cleaning

--- GXPEngine/GXPEngine/Editor/Editor.cs
+++ GXPEngine/GXPEngine/Editor/Editor.cs
@@ -27,7 +27,6 @@ namespace GXPEngine.Editor
 
         Panel ObjectConstructorMenu;
         ConstructorInfo[] constructors;
-        Dictionary<GameObject,TextButton> gameObjectReferences = new Dictionary<GameObject,TextButton>();
 
         public Editor() : base(1200, 600, false, true, true, "GXP Editor")
         { 
@@ -169,46 +168,17 @@ namespace GXPEngine.Editor
         void CreateHierarchy()
         {
             Panel targetWindow = selectedGameObjectMenu;
+            //change 'this' in the line below to whatever the main gameobject is
             editorDisplay = new HierarchyItem(this,0,targetWindow.width - 30, 5,5);
             editorDisplay.ReadChildren();
             targetWindow.AddChild(editorDisplay);
-            //DisplayObject(this,new Vector2(0,0),display,ref items, ref displace);
-            //display.OrganiseChildrenVertical();
         }
         void UpdateHierarchy()
         {
             editorDisplay.UpdateChildren();
             editorDisplay.UpdateDisplay();
         }
-        void DisplayObject(GameObject go, Vector2 pos, Panel display, ref int items, ref int displace)
-        {
-            Panel childDisplay = new Panel(display.width - (int)(pos.x) - 30, 15, 25);
-            if (!gameObjectReferences.ContainsKey(go))
-            {
-                TextButton tb = new TextButton(display.width - (int)(pos.x) - 30, 15, go.GetType().Name, 10);
-                display.AddChild(tb);
-                display.AddChild(childDisplay);
-
-                tb.x = pos.x;
-                tb.y = pos.y;
-                gameObjectReferences.Add(go, tb);
-                displace++;
-            }
-            else
-            {
-                TextButton tb = gameObjectReferences[go];
-                tb.y += displace * 15;
-            }
-            items++;
-            List<GameObject> kiddos = go.GetChildren();
-            for (int i=0; i<kiddos.Count; i++)
-            {
-                GameObject obj = kiddos[i];
-                //Vector2 newPos = new Vector2(pos.x + 15, display.y + items * 15);
-                Vector2 newPos = new Vector2(pos.x + 15, 0);
-                DisplayObject(obj,newPos, childDisplay, ref items, ref displace);
-            }
-        }
+        
         void SetupAddObjectMenu()
         {
             var type = typeof(GameObject);
@@ -255,11 +225,10 @@ namespace GXPEngine.Editor
 
             selectedGameObjectMenu = new SliderPanel(300, height - 10, width - 305, 5);
             selectedGameObjectMenu.SetSliderBar(20, height - 10);
-            //selectedGameObjectMenu.AddElement(new Panel(270, 300));
-            //selectedGameObjectMenu.AddElement(new Panel(270, 300));
-            //selectedGameObjectMenu.AddElement(new Panel(270, 300));
             selectedGameObjectMenu.OrganiseChildrenVertical();
             uiManager.Add(selectedGameObjectMenu);
+
+            //also see UpdateHierarchy() in the Update()
             CreateHierarchy();
         }
 
