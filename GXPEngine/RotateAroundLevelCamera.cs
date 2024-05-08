c89cdc0e8675a1f765082b89da519a7625b4ecd5
caspar LOL <547366@student.saxion.nl>
2024-05-08 15:04:32 +0200

    Better camera

---
+++ GXPEngine/RotateAroundLevelCamera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine.Core;
+
+namespace GXPEngine
+{
+    public class RotateAroundLevelCamera : GameObject
+    {
+        private Camera _cam;
+        public Camera cam { get { return _cam; } }
+        public float distance = 8;
+        Pivot Arm;
+        Vector2 screenRotation = new Vector2(0, 0);
+        Vector2 mouseVelSmoothed = new Vector2(0, 0);
+        bool showCursor;
+        public RotateAroundLevelCamera(Camera cam)
+        {
+            _cam = cam;
+            game.AddChild(cam);
+            Arm = new Pivot();
+            AddChild(Arm);
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(Key.TAB)) showCursor = !showCursor;
+            if (Input.GetMouseButton(0)) showCursor = false;
+            game.ShowMouse(showCursor);
+
+            Arm.z = distance;
+
+            mouseVelSmoothed += (Input.mouseVelocity - mouseVelSmoothed) * Time.deltaTimeS * 5f;
+            screenRotation += mouseVelSmoothed * Time.deltaTimeS;
+            if (screenRotation.y > .3f * Mathf.PI) screenRotation.y = Mathf.PI * .299f;
+            if (screenRotation.y < -.2f * Mathf.PI) screenRotation.y = Mathf.PI * -.199f;
+            screenRotation.x %= 2 * Mathf.PI;
+
+            rotation = Quaternion.FromRotationAroundAxis(Vector3.up, screenRotation.x);
+            Rotate(Quaternion.FromRotationAroundAxis(Vector3.left, screenRotation.y));
+
+            _cam.position = Arm.globalPosition;
+            _cam.rotation = Arm.globalRotation;
+        }
+    }
+}
--- GXPEngine/zGames/Neodymium.cs
+++ GXPEngine/zGames/Neodymium.cs
@@ -9,63 +9,14 @@ namespace GXPEngine
 {
     public class Neodymium : Game
     {
-        Camera cam;
         bool showCursor;
-        public Neodymium() : base(1200, 900, false) 
+        public Neodymium() : base(1000, 750, false, gameName:"Neodymium") 
         {
-            cam = new Camera(new ProjectionMatrix(120, 90, .1f, 100));
-            AddChild(cam);
+            AddChild(new RotateAroundLevelCamera(new Camera(new ProjectionMatrix(100, 75, .1f, 100)) ));
             AddChild(Editor.GameObjectReader.ReadGameObjectTree("level1.gxp3d"));
         }
         void Update()
         {
-            FirstPersonViewUpdate();
-        }
-        void FirstPersonViewUpdate()
-        {
-            if (Input.GetKeyDown(Key.TAB)) showCursor = !showCursor;
-            if (!showCursor && Input.GetMouseButtonDown(0)) showCursor = false;
-            game.ShowMouse(showCursor);
-
-            float msex = Input.mouseX / 800f * Mathf.PI;
-            float msey = Input.mouseY / 600f * Mathf.PI;
-            cam.rotation = (Quaternion.FromRotationAroundAxis(0, 1, 0, msex));
-            cam.Rotate(Quaternion.FromRotationAroundAxis(1, 0, 0, msey));
-            //cam.Rotate(Quaternion.FromRotationAroundAxis(cam.TransformDirection(-1, 0, 0), msey));
-
-            //minecraft creative mode controls
-            if (Input.GetKey(Key.D))
-                cam.Move(Time.deltaTimeS, 0, 0);
-            if (Input.GetKey(Key.A))
-                cam.Move(-Time.deltaTimeS, 0, 0);
-            if (Input.GetKey(Key.W))
-            {
-                Vector3 delta = cam.TransformDirection(0, 0, 1);
-                delta.y = 0;
-                delta = delta.normalized() * (Time.deltaTimeS);
-                cam.position -= delta;
-            }
-            if (Input.GetKey(Key.S))
-            {
-                Vector3 delta = cam.TransformDirection(0, 0, 1);
-                delta.y = 0;
-                delta = delta.normalized() * (Time.deltaTimeS);
-                cam.position += delta;
-            }
-            if (Input.GetKey(Key.LEFT_SHIFT))
-            {
-                Vector3 delta = cam.TransformDirection(0, 1, 0);
-                delta.x = 0; delta.z = 0;
-                delta = delta.normalized() * (Time.deltaTimeS);
-                cam.position -= delta;
-            }
-            if (Input.GetKey(Key.SPACE))
-            {
-                Vector3 delta = cam.TransformDirection(0, 1, 0);
-                delta.x = 0; delta.z = 0;
-                delta = delta.normalized() * (Time.deltaTimeS);
-                cam.position += delta;
-            }
         }
     }
 }
