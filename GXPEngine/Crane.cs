d9eec090551d9ed4b6b9bf21ccb7040c0875196b
Tetroit <shichinov.ivan@gmail.com>
2024-05-10 17:47:28 +0200

    crane if it was really good

--- GXPEngine/Crane.cs
+++ GXPEngine/Crane.cs
@@ -18,14 +18,20 @@ namespace GXPEngine
 
         //magnet has 3 freedom degrees, which can be represented in cylindric coordinate system
         float phi = 0, r = 0, y = 0;
-        float rMin = 2f, rMax = 6f, yMin = 1f, yMax = 5f;
         float cabinCenterOffset;
+        // for values below I use representation in cylindric coordinates vec3 (value_phi, value_r, value_y) speeds correspondingly (phi in rads)
+        public Vector3 lowerLimit = new Vector3(0, 2, 2);
+        public Vector3 upperLimit = new Vector3(0, 8, 14);
+        public Vector3 speedLimits = new Vector3 (1, 0.02f, 0.01f);
+        public Vector3 acceleration = new Vector3(3, 1, 0.1f);
+        public Vector3 velocity;
+
         public Crane(Vector3 groundPos)
         {
             float trunkLength = 8f;
-            float cabinLongEng = rMax+1;
-            float cabinShortEnd = 1;
-            cabinCenterOffset = (cabinLongEng + cabinShortEnd) / 2 - cabinShortEnd;
+            float cabinLongEnd = upperLimit.y + 1;
+            float cabinShortEnd = cabinLongEnd/4;
+            cabinCenterOffset = (cabinLongEnd + cabinShortEnd) / 2 - cabinShortEnd;
 
             trunk = new PhysicsBox("cubeTex.png", groundPos + new Vector3(0, trunkLength / 2, 0),false);
             cabin = new PhysicsBox("cubeTex.png", groundPos + new Vector3(0, trunkLength, cabinCenterOffset), false);
@@ -34,20 +40,25 @@ namespace GXPEngine
             trunk.scaleZ = 0.5f;
             trunk.scaleY = trunkLength/2;
 
-            cabin.scaleZ = (cabinLongEng + cabinShortEnd)/2;
+            cabin.scaleZ = (cabinLongEnd + cabinShortEnd)/2;
             cabin.scaleX = 0.6f;
             cabin.scaleY = 0.4f;
 
             float hookElevation = cabin.scaleY;
-            hook = new PhysicsBox("cubeTex.png", groundPos + new Vector3(0, trunkLength - hookElevation, rMax), false);
-            magnet = new Magnet("test models/monki.obj", "test models/suzanne.png", hook.pos - new Vector3(0, rMin, 0));
+            hook = new PhysicsBox("cubeTex.png", groundPos + new Vector3(0, trunkLength - hookElevation, upperLimit.y), false);
+            magnet = new Magnet("crane/magnet.obj", "crane/magnet.png", hook.pos - new Vector3(0, lowerLimit.y, 0));
+            magnet.rotation = Quaternion.FromRotationAroundAxis(Vector3.left,-Mathf.PI/2);
 
             hook.scale = 0.4f;
             hook.scaleY /= 3;
 
-            magnet.scale = 0.4f;
+            magnet.scale = 0.2f;
+            (magnet.collider as BoxCollider).size = new Vector3(3f, 1f, 3f);
             magnet.mass = 0.16f;
             rope = new Rope(hook, magnet, 0.1f);
+
+            r = upperLimit.y;
+            y = (lowerLimit.z + upperLimit.z) / 2;
         }
         public void AddToGame(Game game)
         {
@@ -58,40 +69,69 @@ namespace GXPEngine
         }
         public void Update()
         {
+            (magnet.collider as BoxCollider).DrawExtents();
+            //phi
             rope.Apply(Time.deltaTimeS);
             rope.Display();
             if (Input.GetKey(Key.E))
             {
-                hook.position += new Vector3(hook.position.z * Time.deltaTimeS, 0, -hook.position.x * Time.deltaTimeS);
-                RotateCabin(Time.deltaTimeS);
+                velocity.x += acceleration.x * Time.deltaTimeS;
+                if (velocity.x > speedLimits.x) velocity.x = speedLimits.x;
             }
             if (Input.GetKey(Key.Q))
             {
-                hook.position -= new Vector3(hook.position.z * Time.deltaTimeS, 0, -hook.position.x * Time.deltaTimeS);
-                RotateCabin(-Time.deltaTimeS);
+                velocity.x -= acceleration.x * Time.deltaTimeS;
+                if (velocity.x < -speedLimits.x) velocity.x = -speedLimits.x;
             }
+            if (!Input.GetKey(Key.Q) && !Input.GetKey(Key.E))
+                velocity.x -= velocity.x * Time.deltaTimeS * 3;
 
+
+
+            //r
             if (Input.GetKey(Key.Z))
             {
-                hook.position += new Vector3(hook.position.x * Time.deltaTimeS / 2, 0, hook.position.z * Time.deltaTimeS / 2);
+                velocity.y += acceleration.y * Time.deltaTimeS;
+                if (velocity.y > speedLimits.y) velocity.y = speedLimits.y;
 
             }
             if (Input.GetKey(Key.X))
             {
-                hook.position -= new Vector3(hook.position.x * Time.deltaTimeS / 2, 0, hook.position.z * Time.deltaTimeS / 2);
+                velocity.y -= acceleration.y * Time.deltaTimeS;
+                if (velocity.y < -speedLimits.y) velocity.y = -speedLimits.y;
+            }
+            if (!Input.GetKey(Key.Z) && !Input.GetKey(Key.X))
+                velocity.y -= velocity.y * Time.deltaTimeS * 3;
 
+            r += velocity.y;
+            if (r<lowerLimit.y) r = lowerLimit.y;
+            if (r>upperLimit.y) r = upperLimit.y;
+
+            RotateCabin(Time.deltaTimeS * velocity.x);
+
+
+
+            //y
+            if (Input.GetKey(Key.F))
+            {
+                velocity.z += acceleration.z * Time.deltaTimeS;
+                if (velocity.z > speedLimits.z) velocity.z = speedLimits.z;
             }
             if (Input.GetKey(Key.R))
             {
-                if (rope.length > 1)
-                    rope.length -= Time.deltaTimeS;
-
+                velocity.z -= acceleration.z * Time.deltaTimeS;
+                if (velocity.z < -speedLimits.z) velocity.z = -speedLimits.z;
             }
-            if (Input.GetKey(Key.F))
-            {
-                rope.length += Time.deltaTimeS;
+            if (!Input.GetKey(Key.Z) && !Input.GetKey(Key.X))
+                velocity.z -= velocity.z * Time.deltaTimeS * 3;
 
-            }
+            y += velocity.z;
+            if (y < lowerLimit.z) y = lowerLimit.z;
+            if (y > upperLimit.z) y = upperLimit.z;
+            rope.length = y;
+
+
+
             //shitty air friction
             magnet.velocity -= magnet.velocity * Time.deltaTimeS * 0.3f;
         }
@@ -109,6 +149,8 @@ namespace GXPEngine
             Vector3 offset = trunk.position ;
             cabin.position = offset + (dir & new Vector3 (cabinCenterOffset, trunk.scaleY, cabinCenterOffset));
             cabin.rotation = Quaternion.FromRotationAroundAxis(Vector3.up, -phi);
+            hook.rotation = cabin.rotation;
+            hook.position = dir & new Vector3 (r, cabin.y - cabin.scaleY, r);
         }
     }
 }
