02d5d4bdefcbe11188c0d86115116bd7d7ad2fb0
caspar LOL <547366@student.saxion.nl>
2024-05-15 12:49:50 +0200

    water drip texture update

--- GXPEngine/Bucket.cs
+++ GXPEngine/Bucket.cs
@@ -21,7 +21,7 @@ namespace GXPEngine
         {
             renderAs.scale = .8f;
             renderAs.y -= 1f;
-            water = new ParticleSystem("neodymium/bucket/water droplet.png", 0, 0, 0, mode: ParticleSystem.Mode.force);
+            water = new ParticleSystem("neodymium/bucket/water drip.png", 0, 0, 0, mode: ParticleSystem.Mode.force);
             AddChild(water);
             water.enabled = false;
             water.forces.Add(new ParticleSystem.GravityForce(Vector3.down*5));
@@ -29,10 +29,6 @@ namespace GXPEngine
             water.endSize = .001f;
         }
 
-        public override void PhysicsUpdate()
-        {
-            base.PhysicsUpdate();
-        }
         void Update()
         {
             water.position = pos;
