3b2dae49bec33627499f26bc5f754cd9ebd8665c
Tetroit <shichinov.ivan@gmail.com>
2024-05-16 20:15:24 +0200

    flags
    sry for delay

--- GXPEngine/Checkpoint.cs
+++ GXPEngine/Checkpoint.cs
@@ -7,15 +7,18 @@ using GXPEngine.Core;
 
 namespace GXPEngine
 {
-    public class Checkpoint : Box
+    public class Checkpoint : DSCFSprite
     {
         static List<Checkpoint> Checkpoints = new List<Checkpoint>();
         protected int order;
-        public Checkpoint(int order) : base("editor/whitePixel.png")
+        public Checkpoint(int order) : base("flag_green.png")
         {
+            CreateBoxCollider();
             collider.isTrigger = true;
             Checkpoints.Add(this);
             this.order = order;
+            SetOrigin(width/2, height/2);
+            size = 0.002f;
         }
 
         public static void AddCheckpointsToPlayer(Player player)
@@ -25,12 +28,15 @@ namespace GXPEngine
             foreach(Checkpoint a in Checkpoints)
             {
                 player.AddCheckpoint(a);
+                if (a.order == Checkpoints.Count - 1)
+                    a._texture = new Texture2D("flag_red.png");
             }
             Checkpoints.Clear();
         }
 
         protected override void RenderSelf(GLContext glContext)
         {
+            base.RenderSelf(glContext);
             //Gizmos.DrawBox(Vector3.zero, new Vector3(1,1,1), this, 0xFFFFFFFF, 1);
         }
     }
